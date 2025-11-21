47f5fa906188646f8a73337cb4b9d0a0b4ded274
WSAL Evan <evan@millers6.com>
2025-11-20 21:41:26 -0500

    Added VoxelWorlds include file

---
+++ include/wv/VoxelWorlds.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <wv/voxel_worlds/Block.h>
+#include <wv/voxel_worlds/BlockRegistry.h>
+#include <wv/voxel_worlds/ChunkData.h>
+#include <wv/voxel_worlds/ChunkManager.h>
+#include <wv/voxel_worlds/ChunkRenderer.h>
+#include <wv/voxel_worlds/WorldGen.h>
